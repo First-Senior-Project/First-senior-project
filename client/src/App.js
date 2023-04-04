d21ea5c3422f29f54dabf80f17ba475af14053d5
jasser-melki <jassermelki838@gmail.com>
2023-04-05 01:26:28 +0200

    backend and lil react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-<h1>hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import SignInClient from './SignInClient';
+import SignUpClient from './SignUpClient';
+import SignInOwner from './SignInOwner';
+import SignUpOwner from './SignUpOwner';
+
+function Home() {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showSignInClient, setShowSignInClient] = useState(false);
+  const [showSignUpClient, setShowSignUpClient] = useState(false);
+  const [showSignInOwner, setShowSignInOwner] = useState(false);
+  const [showSignUpOwner, setShowSignUpOwner] = useState(false);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+    setShowSignInClient(false);
+    setShowSignUpClient(false);
+    setShowSignInOwner(false);
+    setShowSignUpOwner(false);
+  };
+
+  const handleClientClick = () => {
+    setShowSignInClient(true);
+    setShowSignUpClient(false);
+  };
+
+  const handleOwnerClick = () => {
+    setShowSignInClient(false);
+    setShowSignUpClient(false);
+    setShowSignInOwner(true);
+    setShowSignUpOwner(false);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to My Website!</h1>
+      <select value={selectedOption} onChange={handleOptionChange}>
+        <option value="">Select an option</option>
+        <option value="client">For client</option>
+        <option value="owner">For owner</option>
+      </select>
+
+      {selectedOption === "client" && (
+        <>
+          <button onClick={handleClientClick}>Sign In</button>
+          <button onClick={() => setShowSignUpClient(true)}>Sign Up</button>
+          {showSignInClient && <SignInClient />}
+          {showSignUpClient && <SignUpClient />}
+        </>
+      )}
+
+      {selectedOption === "owner" && (
+        <>
+          <button onClick={handleOwnerClick}>Sign In</button>
+          <button onClick={() => setShowSignUpOwner(true)}>Sign Up</button>
+          {showSignInOwner && <SignInOwner />}
+          {showSignUpOwner && <SignUpOwner />}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Home;
