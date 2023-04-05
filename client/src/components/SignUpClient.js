334959a4826e61789bd997d101382e6de3748ea9
jasser-melki <jassermelki838@gmail.com>
2023-04-05 15:50:06 +0200

    signup owner signup client

--- client/src/components/SignUpClient.js
+++ client/src/components/SignUpClient.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 
 function SignUpClient() {
   const [name, setName] = useState('');
+  const [lastname,setLastName]=useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [idowner,setIdOwner]=useState('');
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Submitted!');
+      // make HTTP POST request to server
+      fetch("http://localhost:3001/api/insertClient", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ first_name: name, last_name: lastname, email, password,store_owner_id_owner:idowner })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
   };
 
   return (
@@ -18,12 +29,20 @@ function SignUpClient() {
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
       <label>
+        Last Name:
+        <input type="text" value={lastname} onChange={(e) => setLastName(e.target.value)} />
+      </label>
+      <label>
         Email:
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       </label>
       <label>
         Password:
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Owner ID:
+        <input type="text" value={idowner} onChange={(e) => setIdOwner(e.target.value)} />
       </label>
       <button type="submit">Submit</button>
     </form>
--- client/src/components/SignUpOwner.js
+++ client/src/components/SignUpOwner.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 
 function SignUpOwner() {
   const [name, setName] = useState('');
+  const [lastname, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Submitted!');
+  
+    // make HTTP POST request to server
+    fetch("http://localhost:3001/api/insertOwner", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ first_name: name, last_name: lastname, email, password })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
   };
+  
   
   return (
     <form onSubmit={handleSubmit}>
@@ -18,8 +30,8 @@ function SignUpOwner() {
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
       <label>
-        lastname:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        Last Name:
+        <input type="text" value={lastname} onChange={(e) => setLastName(e.target.value)} />
       </label>
       <label>
         Email:
